fabd463be8f2d6fc242218a28cf381740123fcf9
Saygex <giangthangc3@gmail.com>
2026-06-08 17:56:10 +0700

    cc

--- src/settings/presentation/screens/SettingsScreen.tsx
+++ src/settings/presentation/screens/SettingsScreen.tsx
@@ -76,6 +76,12 @@ function SettingsScreen() {
 
   const handleSettingsItemPress = useCallback(
     async (id: string) => {
+      if (id === 'notifications') {
+        navigation.navigate(ROUTES.MAIN_TABS, {
+          screen: ROUTES.NOTIFICATIONS,
+        });
+      }
+
       if (id === 'earnings') {
         navigation.navigate(ROUTES.EARNINGS);
       }
